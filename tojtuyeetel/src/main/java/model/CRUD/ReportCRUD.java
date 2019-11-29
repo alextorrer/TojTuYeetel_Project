245dc271af3ec9f87517b41a44bfe150f5bcc02a
alextorrer <alextorre0427@gmail.com>
2019-11-28 19:56:17 -0600

    add method to create a report

--- tojtuyeetel/src/main/java/model/CRUD/ReportCRUD.java
+++ tojtuyeetel/src/main/java/model/CRUD/ReportCRUD.java
@@ -1,4 +1,34 @@
 package model.CRUD;
 
+import model.bootstrapper.EMFBootstrapper;
+import model.schemas.Report;
+import model.schemas.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceException;
+
 public class ReportCRUD {
+
+    /**
+     * Method to create a report
+     * @param report
+     */
+    public void createReport(Report report){
+        EntityManager manager = EMFBootstrapper.openEntityManager();
+        EntityTransaction transaction = manager.getTransaction();
+        try {
+            transaction.begin();
+            manager.persist(report);
+            transaction.commit();
+        }
+        catch(PersistenceException e) {
+            transaction.rollback();
+            throw e;
+        }
+        finally {
+            manager.close();
+        }
+    }
+    
 }
