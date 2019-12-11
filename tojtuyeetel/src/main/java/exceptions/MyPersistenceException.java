9512479d80fb619a3f0c4df03c480e491eff1fdc
alextorrer <alextorre0427@gmail.com>
2019-12-10 19:35:58 -0600

    update showException()

--- tojtuyeetel/src/main/java/exceptions/MyPersistenceException.java
+++ tojtuyeetel/src/main/java/exceptions/MyPersistenceException.java
@@ -1,7 +1,6 @@
 
 package exceptions;
 
-import javax.persistence.PersistenceException;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -9,13 +8,14 @@ import javax.swing.JPanel;
  * PersistenceException child 
  * @author alext
  */
-public class MyPersistenceException extends PersistenceException{
+public class MyPersistenceException extends MyException{
     
     /**
      * Method to show an error on the view
      * @param view 
      */
-    public void showError(JPanel view){
-        JOptionPane.showMessageDialog(view, "You must fill every text field" , "ERROR", JOptionPane.ERROR_MESSAGE);
+    @Override
+    public void showException(JPanel view){
+        JOptionPane.showMessageDialog(view, "Persistence error" , "ERROR", JOptionPane.ERROR_MESSAGE);
     }
 }
